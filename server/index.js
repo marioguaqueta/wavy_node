edcd21f21e8f3a39223c281ddc2b26de2b7ad7d1
Mario Guaqueta Restrepo <m_a15@hotmail.es>
2019-10-25 17:22:02 -0300

    finish fixes for messages

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.post('/save',function (req, res){
     console.log('ON SAVE');
-	console.log(req.body["name"]);
+    console.log(req.headers);
+    console.log(req.body);
+
     res.status(200);
     res.send({
         route: 'save'
@@ -23,7 +25,8 @@ app.post('/save',function (req, res){
 
 app.post('/publish',function (req, res){
     console.log('ON PUBLISH');
-	console.log(req.headers);
+    console.log(req.headers);
+    console.log(req.body);
     res.status(200);
     res.send({
         route: 'publish'
@@ -34,7 +37,8 @@ app.post('/publish',function (req, res){
 
 app.post('/validate',function (req, res){
     console.log('ON VALIDATE');
-	console.log(req.headers);
+    console.log(req.headers);
+    console.log(req.body);
     res.status(200);
     res.send({
         route: 'validate'
@@ -44,18 +48,44 @@ app.post('/validate',function (req, res){
 
 
 app.post('/execute',function (req, res){
-    console.log('ON EXECUTE');
-	console.log(req.body);
+    console.log('------------------------------ON EXECUTE----------------------');
+	//console.log(req.headers);
+    //console.log(req.body);
+
+    var inArguments = req.body['arguments'].execute.inArguments;
+    var message = '';
+
+    var datetime = new Date();
+    console.log("INIT TIME: " + datetime);
+
+    var regex = {};
 
 
 
-    //TODO Request to Wavy
+    inArguments.forEach(function(obj) { 
+
+        if (obj.message != undefined) {
+            message = obj.message;
+        }else{
+            regex['%%' + extractFieldName(Object.keys(obj)) + '%%'] =  Object.values(obj);            
+        }
+    });
+
+
+    var decodedMessage = GFG_Fun(regex, message);
+    console.log(decodedMessage);
+
+
+    datetime = new Date();
+    console.log("FINISH TIME: " + datetime);
+
 
     res.status(200);
     res.send({
         route: 'execute'
     });
 });
+
 
 
 app.post('/',function (req, res){
@@ -65,6 +95,22 @@ app.post('/',function (req, res){
 
 
 
+
+function extractFieldName(field) {
+    var stringField = field.toString();
+    var index = stringField.lastIndexOf('.');
+    return field.toString().substring(index + 1);
+}
+
+function GFG_Fun(Obj, str) { 
+    var regex = Obj;
+    var RE = new RegExp(Object.keys(regex).join("|"), "gi"); 
+    var newMessage = str.replace(RE, function(matched) { 
+        return regex[matched]; 
+    });
+
+    return newMessage;
+}
 
 app.listen(PORT, function (){
 	console.log("Esperando requests en el puerto " + PORT);
