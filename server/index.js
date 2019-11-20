453500365317d9306ccaa9125aabc8d9e83458c0
Mario Guaqueta Restrepo <m_a15@hotmail.es>
2019-11-20 17:42:38 -0500

    Body

--- server/index.js
+++ server/index.js
@@ -175,7 +175,7 @@ function sendWavyMessage(decoded){
 
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.get('/', function (req, res){
